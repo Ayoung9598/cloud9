d3c706c354eb48d3441a71a115aae8dd9ef54752
EC2 Default User <ec2-user@ip-10-0-1-39.us-west-2.compute.internal>
2023-08-12 00:39:41 +0000

    Update the app to launch and terminate Amazon EC2 instances

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { useState, useEffect } from 'react';
+// React
+import { useState, useEffect, useMemo, useCallback } from 'react';
+
+// App Component
+import InstanceTable from './InstanceTable';
 
 // Amplify UI
 import '@aws-amplify/ui-react/styles.css';
@@ -7,11 +11,6 @@ import {
     Card,
     Flex,
     Heading,
-    Table,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableCell,
     ToggleButtonGroup,
     ToggleButton,
     View,
@@ -21,91 +20,98 @@ import {
 
 import './App.css';
 
+// Amplify
+import { Amplify, API } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 const theme = {
     name: 'my-theme',
     overrides: [defaultDarkModeOverride],
 };
 
-function App() {
+function App({ user, signOut }) {
     const [colorMode, setColorMode] = useState('system');
-    const [isSigningOut, setIsSigningOut] = useState(false);
+    const [isLaunching, setIsLaunching] = useState(false);
+    const [isRefreshing, setIsRefreshing] = useState(false);
+    const [isTerminating, setIsTerminating] = useState(false);
     const [instances, setInstances] = useState([]);
 
-    // Set initial list of instances
+    const userId = useMemo(
+        () => user.attributes?.email || user.username,
+        [user]
+    );
+
+    // set instances to EC2 instances with a tag of user: id
+    const refreshInstances = useCallback(async () => {
+        setIsRefreshing(true);
+        const apiName = 'vendorApi';
+        const path = '/vendor/instance';
+        const myInit = {
+            queryStringParameters: {
+                owner: userId,
+            },
+        };
+
+        try {
+            const apiResult = await API.get(apiName, path, myInit);
+            const nonTerminatedInstances =
+                apiResult.Reservations?.map((r) => r.Instances)
+                    ?.flat()
+                    ?.filter((r) => r.State?.Name !== 'terminated') || [];
+            setInstances(nonTerminatedInstances);
+        } catch (e) {
+            alert(`Error refreshing instances: ${e}`);
+        }
+
+        setIsRefreshing(false);
+    }, [userId]);
+
+    // load instances initially
     useEffect(() => {
-        const initialInstances = [
-            {
-                InstanceId: 1,
-                LaunchTime: new Date().toLocaleDateString(),
-                PrivateIpAddress: '10.16.1.100',
-                PublicIpAddress: '100.100.100.100',
-                State: {
-                    Name: 'Running',
-                },
-                Tags: [{ Key: 'owner', Value: 'User' }],
-            },
-            {
-                InstanceId: 2,
-                LaunchTime: new Date().toLocaleDateString(),
-                PrivateIpAddress: '10.16.1.101',
-                PublicIpAddress: '100.100.100.101',
-                State: {
-                    Name: 'Running',
-                },
-                Tags: [{ Key: 'owner', Value: 'User' }],
-            },
-        ];
-        setInstances(initialInstances);
-    }, []);
+        refreshInstances();
+    }, [refreshInstances]);
 
-    // Return table rows of instances
-    function InstanceRows() {
-        return instances.map((i) => {
-            return (
-                <TableRow key={i.InstanceId}>
-                    <TableCell>{i.InstanceId}</TableCell>
-                    <TableCell>{i.LaunchTime}</TableCell>
-                    <TableCell>
-                        {i.Tags.find((t) => t.Key === 'owner')?.Value}
-                    </TableCell>
-                    <TableCell>{i.PrivateIpAddress}</TableCell>
-                    <TableCell>{i.PublicIpAddress}</TableCell>
-                    <TableCell>{i.State?.Name}</TableCell>
-                    <TableCell>
-                        <Button
-                            id={i.InstanceId}
-                            size="small"
-                            onClick={(e) => terminateInstance(e.target.id)}
-                        >
-                            Terminate
-                        </Button>
-                    </TableCell>
-                </TableRow>
-            );
-        });
+    // launch ec2 t2.nano instance with latest amazon linux 2 AMI
+    async function handleLaunchInstance() {
+        setIsLaunching(true);
+        const apiName = 'vendorApi';
+        const path = '/vendor/instance/launch';
+        const myInit = {
+            body: {
+                owner: userId,
+            },
+        };
+        try {
+            await API.post(apiName, path, myInit);
+            await refreshInstances();
+        } catch (e) {
+            alert(`Error launching instance: ${e}`);
+        }
+
+        setIsLaunching(false);
     }
 
-    // Add a new row to the instances state
-    function launchInstance() {
-        const maxId = Math.max(...instances.map((i) => i.InstanceId));
-        setInstances([
-            ...instances,
-            {
-                InstanceId: isFinite(maxId) ? maxId + 1 : 1,
-                LaunchTime: new Date().toLocaleDateString(),
-                PrivateIpAddress: '10.16.1.101',
-                PublicIpAddress: '100.100.100.101',
-                State: {
-                    Name: 'Running',
-                },
-                Tags: [{ Key: 'owner', Value: 'User' }],
+    // terminate instance based on ID
+    async function handleTerminateInstance(id) {
+        setIsTerminating(true);
+        const apiName = 'vendorApi';
+        const path = '/vendor/instance/terminate';
+        const myInit = {
+            body: {
+                instanceId: id,
             },
-        ]);
-    }
+        };
 
-    // Remove a row from the instances state based on id
-    function terminateInstance(id) {
-        setInstances(instances.filter((i) => i.InstanceId.toString() !== id));
+        try {
+            await API.post(apiName, path, myInit);
+            await refreshInstances();
+        } catch (e) {
+            alert(`Error terminating instance: ${e}`);
+        }
+
+        setIsTerminating(false);
     }
 
     return (
@@ -119,45 +125,40 @@ function App() {
                     wrap="nowrap"
                     gap="10px"
                 >
-                    <Heading level={2}>Welcome user!</Heading>
-                    <View style={{ marginBottom: '20px' }}>
-                        <Button onClick={() => launchInstance()}>
-                            Launch Instance
+                    <Heading level={2}>
+                        Welcome {user.attributes?.email || user.username}!
+                    </Heading>
+                    <View>
+                        <Button
+                            loadingText="Launching instance"
+                            isLoading={isLaunching}
+                            onClick={() => handleLaunchInstance()}
+                            style={{ marginLeft: '20px' }}
+                        >
+                            Launch
                         </Button>
                         <Button
-                            loadingText="Signing Out..."
-                            isLoading={isSigningOut}
-                            onClick={() => setIsSigningOut(true)}
+                            loadingText="Refreshing..."
+                            isLoading={isRefreshing}
+                            onClick={() => refreshInstances(userId)}
+                            style={{ marginLeft: '20px' }}
+                        >
+                            Refresh
+                        </Button>
+                        <Button
+                            onClick={() => signOut()}
                             style={{ marginLeft: '20px' }}
                         >
                             Sign Out
                         </Button>
                     </View>
-                    {isSigningOut && (
-                        <Button
-                            onClick={() => {
-                                setIsSigningOut(false);
-                            }}
-                        >
-                            Reset
-                        </Button>
-                    )}
-                    <Table highlightOnHover={true} variation="striped">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell as="th">ID</TableCell>
-                                <TableCell as="th">Launch Date</TableCell>
-                                <TableCell as="th">Launched By</TableCell>
-                                <TableCell as="th">Private IP</TableCell>
-                                <TableCell as="th">Public IP</TableCell>
-                                <TableCell as="th">Status</TableCell>
-                                <TableCell as="th"></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            <InstanceRows />
-                        </TableBody>
-                    </Table>
+                    <InstanceTable
+                        instances={instances}
+                        terminateFn={handleTerminateInstance}
+                        terminateAdditional={{
+                            isLoading: isTerminating,
+                        }}
+                    />
                     <ToggleButtonGroup
                         value={colorMode}
                         isExclusive
@@ -173,4 +174,4 @@ function App() {
     );
 }
 
-export default App;
+export default withAuthenticator(App);
